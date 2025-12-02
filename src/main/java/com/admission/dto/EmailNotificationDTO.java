21dcb765e9bf2fdc1e1493de3c1bb0a439e2608c
SravankumrBachu <157828409+SravankumrBachu@users.noreply.github.com>
2025-12-02 17:20:46 +0530

    Update EmailNotificationDTO.java
    remove

--- src/main/java/com/admission/dto/EmailNotificationDTO.java
+++ src/main/java/com/admission/dto/EmailNotificationDTO.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Data Transfer Object for Email Notification
- */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
