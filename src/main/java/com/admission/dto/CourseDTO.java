b9a52338f2a40a3397a4f7f5b357396967694485
SravankumrBachu <157828409+SravankumrBachu@users.noreply.github.com>
2025-12-02 17:20:23 +0530

    Update CourseDTO.java
    remove

--- src/main/java/com/admission/dto/CourseDTO.java
+++ src/main/java/com/admission/dto/CourseDTO.java
@@ -9,9 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.Set;
 
-/**
- * Data Transfer Object for Course
- */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
