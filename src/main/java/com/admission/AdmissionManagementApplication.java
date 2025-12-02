21d34657ce2642f743902cfbec4602509681a5dd
SravankumrBachu <157828409+SravankumrBachu@users.noreply.github.com>
2025-12-02 17:25:00 +0530

    Update AdmissionManagementApplication.java
    remove

--- src/main/java/com/admission/AdmissionManagementApplication.java
+++ src/main/java/com/admission/AdmissionManagementApplication.java
@@ -3,10 +3,7 @@ package com.admission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main Spring Boot Application Entry Point
- * Admission Management System using Spring Boot
- */
+
 @SpringBootApplication
 public class AdmissionManagementApplication {
 
